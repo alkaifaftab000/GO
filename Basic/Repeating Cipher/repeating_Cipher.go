1bac43bb2d0602975801a6a4d086427834c7b288
alkaifaftab000 <alkaif.aftab000@outlook.com>
2025-07-06 22:09:55 +0530

    "repeating_Cipher"

--- Basic/Repeating Cipher/repeating_Cipher.go
+++ Basic/Repeating Cipher/repeating_Cipher.go
@@ -6,10 +6,8 @@ func main() {
 	encryptedStringLength := 0
 	fmt.Scan(&encryptedStringLength)
 	if encryptedStringLength <= 0 || encryptedStringLength >= 56 {
-		fmt.Print("Invalid Encrypted String Length Error Code -1")
 		return
 	}
-
 	encryptedString := ""
 	fmt.Scan(&encryptedString)
 	fmt.Println(DecodeCipher(encryptedString, encryptedStringLength))
@@ -18,15 +16,12 @@ func main() {
 
 func DecodeCipher(encryptedString string, encryptedStringLength int) string {
 	uniqueString := ""
-	fmt.Println("Entered")
-	for i := range encryptedStringLength - 1 {
-		if encryptedString[i] != encryptedString[i+1] {
-			uniqueString += string(encryptedString[i])
-
-		}
+	sum := 0
+	i := 0
+	for sum < encryptedStringLength {
+		uniqueString += string(encryptedString[sum])
+		i++
+		sum += i
 	}
-	fmt.Println("Exited")
-	uniqueString += string(encryptedString[encryptedStringLength-1])
-	fmt.Println(uniqueString)
-	return ""
+	return uniqueString
 }
